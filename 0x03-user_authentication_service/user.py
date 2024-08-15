18a1840349fa796cc3e1cae6e4729494cdf9dc52
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-08-15 22:34:42 +0300

    user-auth-service

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,6 +14,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
-    hashed_password = Column(string(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
