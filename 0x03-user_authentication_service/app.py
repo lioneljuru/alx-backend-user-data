febe08b4fb3830b4c339a1a46cd48c839a203ba6
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-08-15 22:49:36 +0300

    user-auth-service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -85,7 +85,7 @@ def profile() -> str:
     User profile route
     """
     session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({'email': user.email}), 200
     else:
